0c8ac2a7b068aaa708f36581e5103d9b7405c6b8
MeritesKORT <umarumariev03@gmail.com>
2025-12-07 17:08:49 +0800

    Обновил urls.py и manage.py, добавил .gitignore, удалил .pyc

--- StudyTracker/StudyTracker/urls.py
+++ StudyTracker/StudyTracker/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from tracker import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', )
+    path('', views.index, name='index'),
+    path('project/<int:pk>/', views.project_detail, name='project_detail'),
 ]
